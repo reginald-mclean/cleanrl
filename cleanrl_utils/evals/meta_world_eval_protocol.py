aed3de2c446b91753e2126ef0b6174c261dc03c6
Reggie McLean <reginald.mclean@ryerson.ca>
2023-07-12 13:25:54 -0400

    Adding multiprocessed eval protocol

--- cleanrl_utils/evals/meta_world_eval_protocol.py
+++ cleanrl_utils/evals/meta_world_eval_protocol.py
@@ -1,29 +1,62 @@
 import torch
-
-def evaluation_procedure(evaluation_envs, writer, agent, benchmark, keys, update, num_envs, device=torch.device("cpu")):
+import torch.multiprocessing as mp
+def evaluation_procedure(writer, agent, classes, tasks, keys, update, num_envs, device=torch.device("cpu")):
+    workers = []
+    manager = mp.Manager()
+    shared_queue = manager.Queue(num_envs)
+    num_evals = 50
+    eval_rewards = []
     mean_success_rate = 0.0
-    success_rate_dicts = dict()
-    for i, env in enumerate(evaluation_envs):
-        current_task_success_rate = 0.0
-        print(f'Evaluating {keys[i]}')
-        tasks = [task for task in benchmark.train_tasks if task.env_name == keys[i]]
-        for x in range(50):
-            env.set_task(tasks[x])
-            obs, _ = env.reset()
-            count = 0
-            done = False
-            while count < 500 and not done:
-                action, _, _, _ = agent.get_action_and_value(torch.from_numpy(obs).to(torch.float32).to(device).unsqueeze(0))
-                next_obs, reward, terminated, truncated, info = env.step(action.squeeze(0).detach().cpu().numpy())
-                done = truncated or terminated
-                obs = next_obs
-                count += 1
-                if int(info['success']) == 1:
-                    current_task_success_rate += 1
-                    mean_success_rate += 1
-                    done = True
-                if done:
-                    break
-        success_rate_dicts[keys[i]] = float(current_task_success_rate) / 50
-        writer.add_scalar(f"charts/{keys[i]}_success_rate", success_rate_dicts[keys[i]], update - 1)
-    writer.add_scalar("charts/mean_success_rate", float(mean_success_rate) / (num_envs * 50), update - 1)
+    task_results = []
+
+    batch_size = 10 if num_envs >= 10 else num_envs
+    itrs = (num_envs/batch_size)
+    for i in range(itrs):
+        current_keys = keys[i*batch_size:(i+1)*batch_size]
+        for key in current_keys:
+            env_cls = classes[key]
+            env_tasks = [task for task in tasks if task.env_name == key]
+            p = mp.Process(target=multiprocess_eval, args=(env_cls, env_tasks, key, agent, shared_queue, num_evals, device))
+            p.start()
+            workers.apped(p)
+        for process in workers:
+            process.join()
+        for _ in range(len(current_keys)):
+            worker_result = shared_queue.get()
+            if worker_result['eval_rewards'] is not None:
+                eval_rewards += worker_result['eval_rewards']
+                mean_success_rate += worker_result['success_rate']
+                task_results.append((worker_result['task_name'], worker_result['success_rate'], np.mean(worker_result['eval_rewards'])))
+                writer.add_scalar(f"charts/{worker_result['task_name']}_success_rate", worker_result['success_rate']/50, update - 1)
+    writer.add_scalar("charts/mean_success_rate", float(mean_success_rate) / (num_envs * num_evals), update - 1)
+
+def multiprocess_eval(env_cls, env_tasks, env_name, agent, shared_queue, num_evals, device=torch.device("cpu")):
+    env = env_cls()
+    rewards = []
+    success = 0.0
+    for _ in range(num_evals):
+        env.set_task(env_tasks[np.random.randint(0, len(env_tasks))])
+        obs, info = env.reset()
+        count = 0
+        done = False
+        while count < 500 and not done:
+            action, _, _, _ = agent.get_action_and_value(
+                torch.from_numpy(obs).to(torch.float32).to(device).unsqueeze(0))
+            next_obs, reward, terminated, truncated, info = env.step(action.squeeze(0).detach().cpu().numpy())
+            rewards += reward
+            done = truncated or terminated
+            obs = next_obs
+            count += 1
+            if int(info['success']) == 1:
+                success += 1
+                done = True
+            if done:
+                break
+
+    shared_queue.put({
+        'eval_rewards' : rewards,
+        'success_rate' : success,
+        'task_name'    : env_name
+    })
+
+
