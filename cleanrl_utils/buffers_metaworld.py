e2e5bd2c481397c6f0993865e7f8645f34124aca
evangelos-ch <angelos.h@hotmail.com>
2023-07-24 13:49:00 +0100

    Refactor to use map() in MTReplayBuffer and add WIP version of the MetaRL Buffer

--- cleanrl_utils/buffers_metaworld.py
+++ cleanrl_utils/buffers_metaworld.py
@@ -39,9 +39,9 @@ class MultiTaskReplayBuffer:
         self._obs_shape = np.array(envs.single_observation_space.shape).prod()
         self._action_shape = np.array(envs.single_action_space.shape).prod()
 
-        self.clear()  # Init buffer
+        self.reset()  # Init buffer
 
-    def clear(self):
+    def reset(self):
         """Reinitialize the buffer."""
         self.obs = np.zeros((self.capacity, self.num_tasks, self._obs_shape), dtype=np.float32)
         self.actions = np.zeros((self.capacity, self.num_tasks, self._action_shape), dtype=np.float32)
@@ -64,10 +64,6 @@ class MultiTaskReplayBuffer:
 
         self.pos = (self.pos + 1) % self.capacity
 
-    def _get_buffer_tuple(self):
-        """Utility function to lower amount of code necessary to toggle torch usage."""
-        return self.obs, self.actions, self.next_obs, self.dones, self.rewards
-
     def sample(self, single_task_batch_size: int) -> ReplayBufferSamples:
         """Sample a batch of size `single_task_batch_size` for each task.
 
@@ -83,11 +79,72 @@ class MultiTaskReplayBuffer:
             size=(single_task_batch_size,),
         )
 
-        batch = tuple(tuple_item[sample_idx] for tuple_item in self._get_buffer_tuple())
+        batch = (
+            self.obs[sample_idx],
+            self.actions[sample_idx],
+            self.next_obs[sample_idx],
+            self.dones[sample_idx],
+            self.rewards[sample_idx],
+        )
         if self.use_torch:
-            batch = tuple(torch.tensor(item).to(self.device) for item in batch)
+            batch = map(lambda x: torch.tensor(x).to(self.device), batch)
 
         mt_batch_size = single_task_batch_size * self.num_tasks
-        batch = tuple(tuple_item.reshape(mt_batch_size, -1) for tuple_item in batch)
+        batch = map(lambda x: x.reshape(mt_batch_size, -1), batch)
 
         return ReplayBufferSamples(*batch)
+
+
+class MetaLearningReplayBuffer:
+    """A buffer to accumulate trajectories from batches envs for batches of tasks."""
+
+    def __init__(self, num_tasks: int, num_envs_per_task: int, trajectories_per_task: int, max_trajectory_length: int):
+        self.num_tasks = num_tasks
+        self.num_envs_per_task = num_envs_per_task
+        self._combined_batch_size = num_tasks * num_envs_per_task
+        self._target_samples = num_tasks * trajectories_per_task * max_trajectory_length
+
+        self.reset()
+
+    def reset(self):
+        """Reset the buffer."""
+        self.trajectories = [[] for _ in range(self.num_tasks)]
+        self._running_trajectories = [[] for _ in range(self._combined_batch_size)]
+        self._total_samples = 0
+
+    @property
+    def ready(self) -> bool:
+        """Returns whether or not a full batch of trajectories for each task has been sampled.
+
+        Note that this is approximate and won't always have <trajectories_per_task> full trajectories ready.
+        Mirrors how the official ProMP code samples."""
+        return self._total_samples >= self._target_samples
+
+    def sample(self) -> ReplayBufferSamples:
+        """Return the collected trajectories, padded and packed and passed through some extra post processing.
+
+        Mirrors how the official ProMP code prepares data."""
+        # TODO implement all the relevant processing like fitting & applying a baseline, advantage norm etc
+        raise NotImplementedError
+
+    def push(
+        self, obs: npt.NDArray, next_obs: npt.NDArray, action: npt.NDArray, reward: npt.NDArray, done: npt.NDArray
+    ):
+        """Add a 2D batch (meta_batch, batch_per_task, ...) of timesteps to the buffer.
+
+        If an episode finishes here for any of the envs, pop the full trajectory into the trajectories buffer."""
+
+        obs = obs.reshape(self._combined_batch_size, -1).copy()
+        next_obs = next_obs.reshape(self._combined_batch_size, -1).copy()
+        action = action.reshape(self._combined_batch_size, -1).copy()
+        reward = reward.reshape(self._combined_batch_size, 1).copy()
+        done = done.reshape(self._combined_batch_size, 1).copy()
+
+        for i in range(self._combined_batch_size):
+            self._running_trajectories[i].append((obs[i], action[i], next_obs[i], done[i], reward[i]))
+
+            if done[i]:  # pop full trajectories into the trajectories buffer
+                trajectory = ReplayBufferSamples(*map(lambda *xs: np.stack(xs), *self._running_trajectories[i]))
+                self.trajectories[i // self.num_envs_per_task].append(trajectory)
+                self._total_samples += len(trajectory.rewards.shape[0])
+                self._running_trajectories[i] = []
