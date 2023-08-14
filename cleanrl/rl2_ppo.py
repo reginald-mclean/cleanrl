cea4bd0b0e543fdac66759ba20f3ff170868b027
Frank Roeder <fr.coding@proton.me>
2023-08-14 21:43:00 +0200

    [rl2] training works

--- cleanrl/rl2_ppo.py
+++ cleanrl/rl2_ppo.py
@@ -122,18 +122,6 @@ class GRU(nn.Module):
         recurrent_state_masks: torch.Tensor = None,
         device: torch.device = torch.device("cpu"),
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Forward pass for the GRU unit.
-
-        Args:
-            x (torch.Tensor): Input to the GRU.
-            recurrent_states (torch.Tensor): Recurrent state from the previous forward pass.
-            recurrent_state_masks (torch.Tensor): Masks to be applied to the recurrent states.
-            device (torch.device): Device on which to transfer tensors.
-
-        Returns:
-            Tuple
-        """
         if x.size(0) == recurrent_states.size(0):
             if recurrent_state_masks is None:
                 recurrent_state_masks = torch.ones(recurrent_states.shape)
@@ -238,11 +226,8 @@ class GRUActor(nn.Module):
         )
 
     def forward(self, x: torch.Tensor, recurrent_state: torch.Tensor, action=None):
-        try:
-            x, recurrent_state = self.gru(x, recurrent_state, device=x.device)
-            action_mean = self.actor_mean(x)
-        except:
-            import ipdb; ipdb.set_trace()
+        x, recurrent_state = self.gru(x, recurrent_state, device=x.device)
+        action_mean = self.actor_mean(x)
         if x.size()[0] == 10:
             action_logstd = self.actor_logstd.expand_as(action_mean)
         else:
@@ -671,151 +656,98 @@ if __name__ == "__main__":
         meta_episodes.compute_returns(next_value, args.use_gae, args.gamma, args.gae_lambda)
         meta_episode_batch.append(meta_episodes)
 
-        if len(meta_episode_batch) == 8:
-            minibatch_sampler = MetaBatchSampler(meta_episode_batch, device)
-            advantages = minibatch_sampler.returns[:-1] - minibatch_sampler.values[:-1]
-            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
-            minibatches = minibatch_sampler.sample(advantages, 4)
-            for sample in minibatches:
-                (
-                obs_batch,
-                states_actor_batch,
-                states_critic_batch,
-                actions_batch,
-                values_batch,
-                return_batch,
-                dones_batch,
-                old_logprobs_batch,
-                adv_targ,
-                ) = sample
+        if len(meta_episode_batch) == 32:
 
-                import ipdb; ipdb.set_trace()
-                agent_dict = agent.get_action_and_value(obs_batch, states_actor_batch, states_critic_batch)
-                logprob = agent_dict["logprob"]
-                logratio = logprob - old_logprobs_batch
-                ratio = logratio.exp()
+            clipfracs = []
+            for epoch in range(args.update_epochs):
 
-                action = agent_dict["action"].squeeze(1)
+                minibatch_sampler = MetaBatchSampler(meta_episode_batch, device)
+                advantages = minibatch_sampler.returns[:-1] - minibatch_sampler.values[:-1]
+                advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
+                minibatches = minibatch_sampler.sample(advantages, 4)
 
-                value = agent_dict["value"].squeeze(1)
-                actor_state = agent_dict["actor_state"].squeeze(0)
-                critic_state = agent_dict["critic_state"].squeeze(0)
+                for sample in minibatches:
+                    (
+                    obs_batch,
+                    states_actor_batch,
+                    states_critic_batch,
+                    actions_batch,
+                    values_batch,
+                    return_batch,
+                    dones_batch,
+                    old_logprobs_batch,
+                    adv_targ,
+                    ) = sample
 
-                next_value = agent.get_value(next_obs).reshape(1, -1)
-                advantages = torch.zeros_like(rewards).to(device)
-                lastgaelam = 0
-                for t in reversed(range(args.num_steps)):
-                    if t == args.num_steps - 1:
-                        nextnonterminal = 1.0 - next_done
-                        nextvalues = next_value
+                    agent_dict = agent.get_action_and_value(
+                            obs_batch, states_actor_batch, states_critic_batch)
+                    newlogprob = agent_dict["logprob"].sum(-1)
+                    newvalue = agent_dict["value"]
+                    entropy = agent_dict["entropy"]
+                    logratio = newlogprob - old_logprobs_batch
+                    ratio = torch.exp(logratio)
+
+                    with torch.no_grad():
+                        # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                        old_approx_kl = (-logratio).mean()
+                        approx_kl = ((ratio - 1) - logratio).mean()
+                        clipfracs += [
+                            ((ratio - 1.0).abs() > args.clip_coef).float().mean().item()
+                        ]
+
+                    pg_loss1 = - adv_targ * ratio
+                    pg_loss2 = (
+                        -adv_targ  * torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+
+                    )
+                    pg_loss = torch.max(pg_loss1, pg_loss2).mean()
+
+                    newvalue = newvalue.view(-1)
+                    if args.clip_vloss:
+                        v_loss_unclipped = (newvalue - values_batch) ** 2
+                        v_clipped = values_batch + torch.clamp(
+                            newvalue - values_batch,
+                            -args.clip_coef,
+                            args.clip_coef,
+                        )
+                        v_loss_clipped = (v_clipped - return_batch) ** 2
+                        v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
+                        v_loss = 0.5 * v_loss_max.mean()
                     else:
-                        nextnonterminal = 1.0 - dones[t + 1]
-                        nextvalues = values[t + 1]
-                    delta = (
-                        rewards[t] + args.gamma * nextvalues * nextnonterminal - values[t]
-                    )
-                    advantages[t] = lastgaelam = (
-                        delta + args.gamma * args.gae_lambda * nextnonterminal * lastgaelam
-                    )
-                returns = advantages + values
+                        v_loss = 0.5 * ((newvalue - return_batch) ** 2).mean()
 
-                # flatten the batch
-                b_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
-                b_logprobs = logprobs.reshape(-1)
-                b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
-                b_advantages = advantages.reshape(-1)
-                b_returns = returns.reshape(-1)
-                b_values = values.reshape(-1)
+                    entropy_loss = entropy.mean()
+                    loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
 
-                # Optimizing the policy and value network
-                b_inds = np.arange(args.batch_size)
-                clipfracs = []
-                for epoch in range(args.update_epochs):
-                    np.random.shuffle(b_inds)
-                    for start in range(0, args.batch_size, args.minibatch_size):
-                        end = start + args.minibatch_size
-                        mb_inds = b_inds[start:end]
-                        # print(mb_inds)
-                        # print(min(b_inds), max(b_inds))
-                        # print(type(b_obs))
-                        # print(b_obs)
-                        # print(obs.size())
-                        # print(b_obs.size())
-                        # print(b_obs[mb_inds], b_actions[mb_inds])
-                        _, newlogprob, entropy, newvalue = agent.get_action_and_value(
-                            b_obs[mb_inds], b_actions[mb_inds]
-                        )
-                        logratio = newlogprob - b_logprobs[mb_inds]
-                        ratio = logratio.exp()
-
-                        with torch.no_grad():
-                            # calculate approx_kl http://joschu.net/blog/kl-approx.html
-                            old_approx_kl = (-logratio).mean()
-                            approx_kl = ((ratio - 1) - logratio).mean()
-                            clipfracs += [
-                                ((ratio - 1.0).abs() > args.clip_coef).float().mean().item()
-                            ]
-
-                        mb_advantages = b_advantages[mb_inds]
-                        if args.norm_adv:
-                            mb_advantages = (mb_advantages - mb_advantages.mean()) / (
-                                mb_advantages.std() + 1e-8
-                            )
-
-                        # Policy loss
-                        pg_loss1 = -mb_advantages * ratio
-                        pg_loss2 = -mb_advantages * torch.clamp(
-                            ratio, 1 - args.clip_coef, 1 + args.clip_coef
-                        )
-                        pg_loss = torch.max(pg_loss1, pg_loss2).mean()
-
-                        # Value loss
-                        newvalue = newvalue.view(-1)
-                        if args.clip_vloss:
-                            v_loss_unclipped = (newvalue - b_returns[mb_inds]) ** 2
-                            v_clipped = b_values[mb_inds] + torch.clamp(
-                                newvalue - b_values[mb_inds],
-                                -args.clip_coef,
-                                args.clip_coef,
-                            )
-                            v_loss_clipped = (v_clipped - b_returns[mb_inds]) ** 2
-                            v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
-                            v_loss = 0.5 * v_loss_max.mean()
-                        else:
-                            v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
-
-                        entropy_loss = entropy.mean()
-                        loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef
-
-                        optimizer.zero_grad()
-                        loss.backward()
-                        nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
-                        optimizer.step()
+                    optimizer.zero_grad()
+                    loss.backward()
+                    nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
+                    optimizer.step()
 
                     if args.target_kl is not None:
                         if approx_kl > args.target_kl:
                             break
 
-                y_pred, y_true = b_values.cpu().numpy(), b_returns.cpu().numpy()
-                var_y = np.var(y_true)
-                explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
+                    y_pred, y_true = values_batch.cpu().numpy(), return_batch.cpu().numpy()
+                    var_y = np.var(y_true)
+                    explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
-            # TRY NOT TO MODIFY: record rewards for plotting purposes
-            writer.add_scalar(
-                "charts/learning_rate", optimizer.param_groups[0]["lr"], global_step
-            )
-            writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
-            writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
-            writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
-            writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
-            writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
-            writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
-            writer.add_scalar("losses/explained_variance", explained_var, global_step)
+                    # TRY NOT TO MODIFY: record rewards for plotting purposes
+                    writer.add_scalar(
+                        "charts/learning_rate", optimizer.param_groups[0]["lr"], global_step
+                    )
+                    writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
+                    writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
+                    writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
+                    writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
+                    writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
+                    writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
+                    writer.add_scalar("losses/explained_variance", explained_var, global_step)
+                    print("SPS:", int(global_step / (time.time() - start_time)))
+                    writer.add_scalar(
+                        "charts/SPS", int(global_step / (time.time() - start_time)), global_step
+                    )
 
-        print("SPS:", int(global_step / (time.time() - start_time)))
-        writer.add_scalar(
-            "charts/SPS", int(global_step / (time.time() - start_time)), global_step
-        )
         if global_step % 500 == 0 and global_episodic_return:
             writer.add_scalar(
                 "charts/mean_episodic_return",
@@ -830,6 +762,7 @@ if __name__ == "__main__":
             print(
                 f"global_step={global_step}, mean_episodic_return={np.mean(global_episodic_return)}"
             )
+        global_step +=1
 
     envs.close()
     writer.close()
