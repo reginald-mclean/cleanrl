[promp,maml] Fix GAE, switch to Adam for inner optimiser and total env num steps computation

cleanrl/maml_trpo_metaworld_jax.py
@@ -5,7 +5,7 @@ import random
 import time
 from distutils.util import strtobool
 from functools import partial
-from typing import List, Optional, Tuple, Type
+from typing import Callable, List, Optional, Tuple, Type
 
 os.environ[
     "XLA_PYTHON_CLIENT_PREALLOCATE"
@@ -53,8 +53,8 @@ def parse_args():
 
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="ML10", help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=1001,
-        help="total number of meta gradient steps")
+    parser.add_argument("--total-timesteps", type=int, default=15_000_000,
+        help="total number timesteps to collect from the environment")
     parser.add_argument("--max-episode-steps", type=int, default=500,
         help="maximum number of timesteps in one episode during training")
     parser.add_argument("--gamma", type=float, default=0.99,
@@ -68,13 +68,12 @@ def parse_args():
         help="the number of trajectories to collect per task in the meta batch")
     parser.add_argument("--num-layers", type=int, default=2, help="the number of hidden layers in the MLP")
     parser.add_argument("--hidden-dim", type=int, default=512, help="the dimension of each hidden layer in the MLP")
-    parser.add_argument("--inner-lr", type=float, default=0.05, help="the inner (adaptation) step size")
-    parser.add_argument("--meta-lr", type=float, default=1e-3, help="the meta-policy gradient step size")
+    parser.add_argument("--inner-lr", type=float, default=0.1, help="the inner (adaptation) step size")
     parser.add_argument("--num-inner-gradient-steps", type=int, default=1,
         help="number of inner / adaptation gradient steps")
     # TRPO
     parser.add_argument("--delta", type=float, default=0.01,
-        help="the value the KL divergence is constrianed to in TRPO")
+        help="the value the KL divergence is constrained to in TRPO")
     parser.add_argument("--cg-iters", type=int, default=10,
         help="the maximum number of iterations of the conjugate gradient algorithm")
     parser.add_argument("--backtrack-ratio", type=float, default=0.8,
@@ -244,7 +243,7 @@ def inner_step(policy: TrainState, rollouts: Rollout) -> TrainState:
         return -(log_probs * rollouts.advantages).mean()
 
     grads = jax.grad(inner_opt_objective)(policy.params)
-    updated_policy = policy.apply_gradients(grads=grads)  # Inner gradient step, SGD
+    updated_policy = policy.apply_gradients(grads=grads)  # Inner gradient step
 
     return updated_policy
 
@@ -278,7 +277,7 @@ def outer_step(
         outer_objective = likelihood_ratio * rollouts.advantages
         return -outer_objective.mean()
 
-    # TRPO
+    # TRPO, outer gradient step
     rollouts = all_rollouts[-1]
 
     def kl_constraint(params: FrozenDict, inputs: Rollout, targets: distrax.Distribution):
@@ -375,11 +374,11 @@ class LinearFeatureBaseline:
         return np.stack(coeffs)
 
     @classmethod
-    def fit_baseline(cls, rollouts: Rollout) -> np.ndarray:
+    def fit_baseline(cls, rollouts: Rollout) -> Callable[[Rollout], np.ndarray]:
         coeffs = cls._fit_baseline(rollouts.observations, rollouts.returns)
 
-        def baseline(obs: np.ndarray) -> np.ndarray:
-            features = cls._extract_features(obs, reshape=False)
+        def baseline(rollouts: Rollout) -> np.ndarray:
+            features = cls._extract_features(rollouts.observations, reshape=False)
             return features @ coeffs
 
         return baseline
@@ -393,6 +392,7 @@ class MAMLTRPO:
         hidden_dim: int,
         init_key: jax.random.PRNGKey,
         init_obs: ArrayLike,
+        inner_lr: float,
         delta: float,
         cg_iters: int,
         backtrack_ratio: float,
@@ -406,6 +406,8 @@ class MAMLTRPO:
             "num_actions": np.prod(envs.single_action_space.shape),
         }
 
+        self.inner_lr = inner_lr
+
         # Init general parameters theta
         theta = MetaVectorPolicy.init_single(**self.network_args, rng=init_key, init_args=[init_obs])
         self.init_multitask_policy(self.num_tasks, theta)
@@ -414,7 +416,6 @@ class MAMLTRPO:
             apply_fn=lambda x: x,
             inner_train_state=self.policy,
             params=theta,
-            tx=optax.adam(args.meta_lr),  # outer optimizer
         )
 
         # TRPO
@@ -431,11 +432,16 @@ class MAMLTRPO:
         # Init a vectorized policy, running a separate set of parameters for each task
         # The initial parameters for each task are all the same
         policy_network = MetaVectorPolicy(n_tasks=num_tasks, **self.network_args)
-        self.policy = TrainState.create(
-            apply_fn=policy_network.apply,
-            params=MetaVectorPolicy.expand_params(params, num_tasks),
-            tx=optax.sgd(learning_rate=args.inner_lr),  # inner optimizer
-        )
+        if not self.policy:
+            self.policy = TrainState.create(
+                apply_fn=policy_network.apply,
+                params=MetaVectorPolicy.expand_params(params, num_tasks),
+                tx=optax.adam(learning_rate=self.inner_lr),  # inner optimizer
+            )
+        else:
+            self.policy = self.policy.replace(
+                apply_fn=policy_network.apply, params=policy_network.expand_params(params, num_tasks)
+            )
 
     def adapt(self, rollouts: Rollout) -> None:
         self.policy = inner_step(self.policy, rollouts)
@@ -559,8 +565,11 @@ if __name__ == "__main__":
     start_time = time.time()
 
     # TRY NOT TO MODIFY: start the game
-    for global_step in range(args.total_timesteps):  # Outer step
-        print(f"Step {global_step}")
+    steps_per_iter = args.meta_batch_size * args.rollouts_per_task * args.max_episode_steps
+    n_iters = args.total_timesteps // steps_per_iter
+    for _iter in range(n_iters):  # Outer step
+        global_step = _iter * steps_per_iter
+        print(f"Iteration {_iter}, Global num of steps {global_step}")
         agent.init_multitask_policy(envs.num_envs, agent.train_state.params)
         all_rollouts: List[Rollout] = []
 

cleanrl/promp_metaworld_jax.py
@@ -5,7 +5,7 @@ import random
 import time
 from distutils.util import strtobool
 from functools import partial
-from typing import List, Optional, Tuple, Type
+from typing import List, Optional, Tuple, Type, Callable
 
 os.environ[
     "XLA_PYTHON_CLIENT_PREALLOCATE"
@@ -52,7 +52,7 @@ def parse_args():
 
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="ML10", help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=1001,
+    parser.add_argument("--total-timesteps", type=int, default=15_000_000,
         help="total number of meta gradient steps")
     parser.add_argument("--max-episode-steps", type=int, default=500,
         help="maximum number of timesteps in one episode during training")
@@ -248,7 +248,7 @@ def inner_step(
         return -(ratio * rollouts.advantages).mean(), kl
 
     grads, kl = jax.grad(inner_opt_objective, has_aux=True)(policy.params)
-    updated_policy = policy.apply_gradients(grads=grads)  # Inner gradient step, SGD
+    updated_policy = policy.apply_gradients(grads=grads)  # Inner gradient step
 
     return updated_policy, kl
 
@@ -346,11 +346,11 @@ class LinearFeatureBaseline:
         return np.stack(coeffs)
 
     @classmethod
-    def fit_baseline(cls, rollouts: Rollout) -> np.ndarray:
+    def fit_baseline(cls, rollouts: Rollout) -> Callable[[Rollout], np.ndarray]:
         coeffs = cls._fit_baseline(rollouts.observations, rollouts.returns)
 
-        def baseline(obs: np.ndarray) -> np.ndarray:
-            features = cls._extract_features(obs, reshape=False)
+        def baseline(rollouts: Rollout) -> np.ndarray:
+            features = cls._extract_features(rollouts.observations, reshape=False)
             return features @ coeffs
 
         return baseline
@@ -400,11 +400,16 @@ class ProMP:
         # Init a vectorized policy, running a separate set of parameters for each task
         # The initial parameters for each task are all the same
         policy_network = MetaVectorPolicy(n_tasks=num_tasks, **self.network_args)
-        self.policy = TrainState.create(
-            apply_fn=policy_network.apply,
-            params=MetaVectorPolicy.expand_params(params, num_tasks),
-            tx=optax.sgd(learning_rate=args.inner_lr),  # inner optimizer
-        )
+        if not self.policy:
+            self.policy = TrainState.create(
+                apply_fn=policy_network.apply,
+                params=MetaVectorPolicy.expand_params(params, num_tasks),
+                tx=optax.adam(learning_rate=self.inner_lr),  # inner optimizer
+            )
+        else:
+            self.policy = self.policy.replace(
+                apply_fn=policy_network.apply, params=policy_network.expand_params(params, num_tasks)
+            )
 
     def adapt(self, rollouts: Rollout) -> None:
         self.policy, _ = inner_step(self.policy, rollouts)
@@ -521,8 +526,11 @@ if __name__ == "__main__":
     start_time = time.time()
 
     # TRY NOT TO MODIFY: start the game
-    for global_step in range(args.total_timesteps):  # Outer step
-        print(f"Step {global_step}")
+    steps_per_iter = args.meta_batch_size * args.rollouts_per_task * args.max_episode_steps
+    n_iters = args.total_timesteps // steps_per_iter
+    for _iter in range(n_iters):  # Outer step
+        global_step = _iter * steps_per_iter
+        print(f"Iteration {_iter}, Global num of steps {global_step}")
         agent.init_multitask_policy(envs.num_envs, agent.train_state.params)
         all_rollouts: List[Rollout] = []
 

cleanrl_utils/buffers_metaworld.py
@@ -184,14 +184,32 @@ class MultiTaskRolloutBuffer:
         """Discounted cumulative sum.
 
         See https://docs.scipy.org/doc/scipy/reference/tutorial/signal.html#difference-equation-filtering"""
-        # From garage, modified to work on multi-dimensional arrays
-        return scipy.signal.lfilter([1], [1, float(-discount)], rewards[..., ::-1], axis=-1)[..., ::-1]
+        # From garage, modified to work on multi-dimensional arrays, and column reward vectors
+        reshape = rewards.shape[-1] == 1
+        if reshape:
+            rewards = rewards.reshape(rewards.shape[:-1])
+        returns = scipy.signal.lfilter([1], [1, float(-discount)], rewards[..., ::-1], axis=-1)[..., ::-1]
+        return returns if not reshape else returns.reshape(*returns.shape, 1)
 
     def _compute_advantage(self, rewards: npt.NDArray, baselines: npt.NDArray, gamma: float, gae_lambda: float):
+        assert rewards.shape == baselines.shape, "Rewards and baselines must have the same shape."
+        reshape = rewards.shape[-1] == 1
+        if reshape:
+            rewards = rewards.reshape(rewards.shape[:-1])
+            baselines = baselines.reshape(baselines.shape[:-1])
+
         # From ProMP's advantage computation, modified to work on multi-dimensional arrays
         baselines = np.append(baselines, np.zeros((*baselines.shape[:-1], 1)), axis=-1)
         deltas = rewards + gamma * baselines[..., 1:] - baselines[..., :-1]
-        return self._get_returns(deltas, gamma * gae_lambda)
+        advantages = self._get_returns(deltas, gamma * gae_lambda)
+        return advantages if not reshape else advantages.reshape(*advantages.shape, 1)
+
+    def _normalize_advantages(self, advantages: npt.NDArray) -> npt.NDArray:
+        axis = tuple(np.arange(advantages.ndim)[1:]) if (advantages.ndim > 2 and advantages.shape[-1] == 1) else None
+        mean = np.mean(advantages, axis=axis, keepdims=axis is not None)
+        var = np.var(advantages, axis=axis, keepdims=axis is not None)
+
+        return (advantages - mean) / (var + 1e-8)
 
     def _to_torch(self, rollouts: Rollout) -> Rollout:
         return Rollout(*map(lambda x: torch.tensor(x).to(self._device), rollouts))  # type: ignore
@@ -250,11 +268,7 @@ class MultiTaskRolloutBuffer:
 
         # 4.1) (Optional) Normalize advantages
         if normalize_advantages:
-            advantages = task_rollouts.advantages
-            norm_advantages = (advantages - np.mean(advantages, axis=1, keepdims=True)) / (
-                np.std(advantages, axis=1, keepdims=True) + 1e-8
-            )
-            task_rollouts = task_rollouts._replace(advantages=norm_advantages)
+            task_rollouts = task_rollouts._replace(advantages=self._normalize_advantages(task_rollouts.advantages))
 
         return self._to_torch(task_rollouts) if self._use_torch else task_rollouts
 
@@ -299,7 +313,7 @@ class MultiTaskRolloutBuffer:
         # 2.2) Apply baseline
         # NOTE baseline is responsible for any data conversions / moving to the GPU
         assert baseline is not None, "You must provide a baseline function, or a fit_baseline that returns one."
-        baselines = baseline(all_rollouts.observations)
+        baselines = baseline(all_rollouts)
 
         # 3) Compute advantages
         advantages = self._compute_advantage(all_rollouts.rewards, baselines, gamma, gae_lambda)  # type: ignore